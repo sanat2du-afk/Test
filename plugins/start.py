bee9ee1f6796a2f9b41c7f3a87df9e72bce93ed1
sanat2du-afk <sanat2du@gmail.com>
2026-03-10 15:20:16 +0530

    Create start.py

---
+++ plugins/start.py
@@ -0,0 +1,55 @@
+from pyrogram import Client, filters
+from pyrogram.types import InlineKeyboardMarkup, InlineKeyboardButton
+from database.users import add_user
+import config
+
+START_PIC = "assets/start.jpg"
+
+
+@Client.on_message(filters.command("start"))
+async def start_command(client, message):
+
+    user_id = message.from_user.id
+
+    await add_user(user_id)
+
+    buttons = InlineKeyboardMarkup(
+        [
+            [
+                InlineKeyboardButton(
+                    "🫂 Add Me To Your Group",
+                    url=f"https://t.me/{(await client.get_me()).username}?startgroup=true"
+                )
+            ],
+            [
+                InlineKeyboardButton(
+                    "🔎 Search Movies",
+                    url=f"https://t.me/{(await client.get_me()).username}"
+                )
+            ],
+            [
+                InlineKeyboardButton(
+                    "🎄 About",
+                    url="https://t.me/your_about_link"
+                )
+            ]
+        ]
+    )
+
+    text = """
+🎬 Welcome to Movie AutoFilter Bot
+
+You can search movies easily and get download links with poster and details.
+
+Features:
+• Smart Movie Search
+• IMDb Poster & Rating
+• Multiple Download Quality
+• Fast Database Search
+"""
+
+    await message.reply_photo(
+        photo=START_PIC,
+        caption=text,
+        reply_markup=buttons
+  )
